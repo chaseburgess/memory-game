a7de09da7de6e83fbe98b2588da669d946b9140c
cburgess <sturm0674@gmail.com>
2023-09-28 02:19:40 -0600

    board- display

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
-/*----- constants -----*/
-// const CARDS = [
-// {img: 'img_path', value 'a' matched : flase}
-// ]
-const CARDS = ['a', 'a'];
-const WINNING_SCORE = CARDS.length;
+ /*----- constants -----*/
+const CARDS = [
+{img: 'img_path', value: 'Yoda', matched : false},
+{img: 'img_path', value: 'Yoda', matched : false},
+{img: 'img_path', value: 'Luke', matched : false},
+{img: 'img_path', value: 'Luke', matched : false},
+{img: 'img_path', value: 'Darth', matched : false},
+{img: 'img_path', value: 'Darth', matched : false},
+{img: 'img_path', value: 'Obi-Wan', matched : false},
+{img: 'img_path', value: 'Obi-Wan', matched : false},
+{img: 'img_path', value: 'Palpatine', matched : false},
+{img: 'img_path', value: 'Palpatine', matched : false},
+{img: 'img_path', value: 'R2D2', matched : false},
+{img: 'img_path', value: 'R2D2', matched : false},
+]
+// const CARDS = ['a', 'a'];
+const WINNING_SCORE = CARDS.length / 2;
 const INITAL_SCORE = 0;
-// const CARD_BACK = 
-
-	/*----- state variables -----*/
+// const MATCH_DISPLAY = img: '';
+/*----- state variables -----*/
 // this is an array that holds the values of the clicked cards
 let cardsMatched;
 let score;
 
 	/*----- cached elements  -----*/
-// const guessesEl = document.querySelector();
+// const scoreDisplayEl = document.querySelector();
 const startButtonEl = document.getElementById('start-button');
 // grab the score display element by id and save it to a variable named scoreDisplayEl
 const gameBoardEl = document.getElementById('game-board');
+const scoreDisplayEl = document.getElementById('score-display');
 
 	/*----- event listeners -----*/
 startButtonEl.addEventListener('click', init);
@@ -32,8 +43,9 @@ function init() {
 
 function renderCards() {
     CARDS.forEach(card => {
+        console.log(card);
         let cardEl = document.createElement('div');
-        cardEl.innerText = card;
+        cardEl.innerText = card.value;
         cardEl.classList.add('card-element')
         cardEl.addEventListener('click', handleCardClick);
         gameBoardEl.append(cardEl);
@@ -42,30 +54,56 @@ function renderCards() {
     // apend cardEl to game board
 };
 
+// function handleCardClick() {
+// //change background color style of the clicked element (in dom element in js)
+// console.log(this);
+//     cardsMatched.push(this.innerText); 
+//     if (cardsMatched.length === 2) {
+//       if (cardsMatched[0] === cardsMatched[1]) {
+//         score++;
+//       }
+//     //   else statment needed here if they dont match remove background styling
+//       render();
+//         cardsMatched = [];
+//     }
+// }
+
 function handleCardClick() {
-// when we click the card push it to the cardsMatched array
-// if the length of the cardsMatched arrray is 2
-    // check if the cards matched array at index 0 === cardsMatched array at index 1
-        //increment score by 1 
-// call render function             
+  // Change the background color style of the clicked element
+  this.style.backgroundColor = 'greenyellow';
+
+  // Add the inner text of the clicked card to the 'cardsMatched' array
+  cardsMatched.push(this.innerText); 
+
+  if (cardsMatched.length === 2) {
+    if (cardsMatched[0] === cardsMatched[1]) {
+      score++;
+    } else {
+      // Cards do not match, remove background styling
+      setTimeout(() => {
+        this.style.backgroundColor = 'red'; // Reset to default background color
+        // You can add additional logic here if needed
+      }, 1000); // Change the timeout value as needed
+
+      // You may want to do something else when cards don't match
+    }
+    
+    // Call the 'render()' function to update the game display
+    render();
+    
+    // Reset the 'cardsMatched' array
+    cardsMatched = [];
+  }
 }
 
 
-
 function render() {
-// set the innerText of the scoreDisplayEl to the score variable
-
+    scoreDisplayEl.innerText = `Score: ${score}`;
 };
 
-// img;
-// src;
-// gameStatus;
-// cardShuffle;
-// buttons;
+//function that checks for wins
+//define winning condition 
+//if user has won, else lost
 
 
-
-// matches;
-// cardChoice;
-// wrongChoice;
-// correctChoice;
+//repay button? 
