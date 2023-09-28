28377282d05b62067c1c7964ed5306fc4471dd44
cburgess <sturm0674@gmail.com>
2023-09-28 09:03:59 -0600

    README updated

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# memory-game
+# may-the-match-be-with-you
+
+![]https://sm.ign.com/t/ign_ap/feature/s/star-wars-/star-wars-school-the-history-of-the-lightsaber_sacd.1280.jpg
+
+[click to play](https://github.com/chaseburgess/memory-game)
+
+## description 
+
+'May The Match Be With You' is an outerspace themed version of the classic 'Memory Match Game'. The goal is to click on a tiles and remeber where its "match" is. 
+
+## screesnshots
+
+  |   Description | Screenshot | 
+  |:-------------:| -----------|
+
+  |start of game|<img src="https://imgur.com/a/82lgBM4"> |
+
+  |Couple of moves| <img src="https://imgur.com/l0dNBR5"> |
+
+  |Winner| <img src="https://imgur.com/pmjiKTa"> |
+
+## technologies used
+
+![JavaScript](https://img.shields.io/badge/-JavaScript-05122A?style=flat&logo=javascript)
+
+![VSCode](https://img.shields.io/badge/-VS_Code-05122A?style=flat&logo=visualstudio)
+
+![HTML5](https://img.shields.io/badge/-HTML5-05122A?style=flat&logo=html5)
+
+![CSS3](https://img.shields.io/badge/-CSS-05122A?style=flat&logo=css3)
+
+![Git](https://img.shields.io/badge/-Git-05122A?style=flat&logo=git)
+
+![Github](https://img.shields.io/badge/-GitHub-05122A?style=flat&logo=github)
+
+## getting started 
+
+<details open>
+  <summary> Instructions </summary>
+
+    1. Press Start Game
+
+    2. Click on a tile. 
+
+    3. Click on another title.
+
+    4. Find eavh tiles match.
+
+</details>
+
+## upcoming features
+
+- [X] Actually hidden tiles
+
+- [X] Fun image appears when you win or lose the game.
+
+- [X] Gives user the option to pick a new language
+
+- [X] More tiles options
+
+- [X] Images for the tiles
+
+- [X] Hints
+
+- [X] Replay button
--- js/main.js
+++ js/main.js
@@ -1,113 +1,70 @@
-// *----- constants -----*/
 const CARDS = [
-{img: 'img_path', value: 'Yoda', matched : false},
-{img: 'img_path', value: 'Yoda', matched : false},
-{img: 'img_path', value: 'Luke', matched : false},
-{img: 'img_path', value: 'Luke', matched : false},
-{img: 'img_path', value: 'Darth', matched : false},
-{img: 'img_path', value: 'Darth', matched : false},
-]
-// {img: 'img_path', value: 'Obi-Wan', matched : false},
-// {img: 'img_path', value: 'Obi-Wan', matched : false},
-// {img: 'img_path', value: 'Palpatine', matched : false},
-// {img: 'img_path', value: 'Palpatine', matched : false},
-// {img: 'img_path', value: 'R2D2', matched : false},
-// {img: 'img_path', value: 'R2D2', matched : false},
+    { img: 'img_path', value: 'Yoda', matched: false },
+    { img: 'img_path', value: 'Yoda', matched: false },
+    { img: 'img_path', value: 'Luke', matched: false },
+    { img: 'img_path', value: 'Luke', matched: false },
+    { img: 'img_path', value: 'Darth', matched: false },
+    { img: 'img_path', value: 'Darth', matched: false },
+];
 
-// const CARDS = ['a', 'a'];
 const WINNING_SCORE = CARDS.length / 2;
-const INITAL_SCORE = 0;
-// const MATCH_DISPLAY = img: '';
-/*----- state variables -----*/
-// this is an array that holds the values of the clicked cards
+const INITIAL_SCORE = 0;
+
 let cardsMatched;
 let score;
 
-	/*----- cached elements  -----*/
-// const scoreDisplayEl = document.querySelector();
 const startButtonEl = document.getElementById('start-button');
-// grab the score display element by id and save it to a variable named scoreDisplayEl
 const gameBoardEl = document.getElementById('game-board');
 const scoreDisplayEl = document.getElementById('score-display');
 const messageEl = document.getElementById('message');
 
-	/*----- event listeners -----*/
 startButtonEl.addEventListener('click', init);
 
-	/*----- functions -----*/
 function init() {
-    score = INITAL_SCORE;
+    score = INITIAL_SCORE;
     cardsMatched = [];
     startButtonEl.disabled = true;
     render();
     renderCards();
-};
+    renderButtons();
+}
 
 function renderCards() {
     CARDS.forEach(card => {
         console.log(card);
         let cardEl = document.createElement('div');
         cardEl.innerText = card.value;
-        cardEl.classList.add('card-element')
+        cardEl.classList.add('card-element');
         cardEl.addEventListener('click', handleCardClick);
         gameBoardEl.append(cardEl);
-    })
-    // create card
-    // apend cardEl to game board
-};
-
-// function handleCardClick() {
-// //change background color style of the clicked element (in dom element in js)
-// console.log(this);
-//     cardsMatched.push(this.innerText); 
-//     if (cardsMatched.length === 2) {
-//       if (cardsMatched[0] === cardsMatched[1]) {
-//         score++;
-//       }
-//     //   else statment needed here if they dont match remove background styling
-//       render();
-//         cardsMatched = [];
-//     }
-// }
-
-function handleCardClick() {
-  // Change the background color style of the clicked element
-  this.style.backgroundColor = 'greenyellow';
-
-  // Add the inner text of the clicked card to the 'cardsMatched' array
-  cardsMatched.push(this.innerText); 
-
-  if (cardsMatched.length === 2) {
-    if (cardsMatched[0] === cardsMatched[1]) {
-      score++;
-    } else {
-      // Cards do not match, remove background styling
-      setTimeout(() => {
-        this.style.backgroundColor = 'red'; // Reset to default background color
-        // You can add additional logic here if needed
-      }, 1000); // Change the timeout value as needed
-
-      // You may want to do something else when cards don't match
-    }
-    
-    // Call the 'render()' function to update the game display
-    render();
-    cardsMatched = [];
-    checkWin();
-  }
+    });
 }
 
+function handleCardClick() {
+    this.style.backgroundColor = 'greenyellow';
+    cardsMatched.push(this.innerText);
+    if (cardsMatched.length === 2) {
+        if (cardsMatched[0] === cardsMatched[1]) {
+            score++;
+        } else {
+            setTimeout(() => {
+                this.style.backgroundColor = 'red';
+            }, 1000);
+        }
+        render();
+        cardsMatched = [];
+        checkWin();
+    }
+}
 
 function render() {
     scoreDisplayEl.innerText = `Score: ${score}`;
-};
-
-//function that checks for wins
-//if user has won, else lost
-function checkWin() {
-    if(score === WINNING_SCORE) {
-        messageEl.innerText = "Congrats! You won!";
-    }
 }
 
-//repay button? 
+function checkWin() {
+    if (score === WINNING_SCORE) {
+        messageEl.innerText = 'Congrats! You won!';
+    } else {
+        // Handle other conditions or messages if needed
+    }
+}
